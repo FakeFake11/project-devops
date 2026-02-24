1acaaa73f940f7fe468851988dbda001d6d8f764
Henri Hollanti <henri.hollanti@hotmail.com>
2026-02-24 12:59:48 +0200

    feat(jframe): Add private constructor for App

--- app/src/main/java/tictactoe/App.java
+++ app/src/main/java/tictactoe/App.java
@@ -3,6 +3,11 @@ package tictactoe;
 import javax.swing.Jframe;
 
 public final class App {
+
+    private App() {
+
+    }
+
     public static void main (final String[] args) {
         System.out.println("Hello world");
     }
