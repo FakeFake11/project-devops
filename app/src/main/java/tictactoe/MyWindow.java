25ed7fcd12b1875fda3b10c28482cd9584ebeb53
Henri Hollanti <henri.hollanti@hotmail.com>
2026-02-24 13:38:26 +0200

    feat(jframe): Modify MyWindow constructor, Add constants WINDOW_HEIGHT and WINDOW_WIDTH

--- app/src/main/java/tictactoe/MyWindow.java
+++ app/src/main/java/tictactoe/MyWindow.java
@@ -4,8 +4,12 @@ import javax.swing.JFrame;
 
 public final class MyWindow extends JFrame{
 
+    public static final int WINDOW_WIDTH = 400;
+    public static final int WINDOW_HEIGHT = 400;
+
     public MyWindow() {
         super("window");
-        setSize(400, 400);
+        setSize(WINDOW_HEIGHT, WINDOW_WIDTH);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
