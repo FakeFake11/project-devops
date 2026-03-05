3e5b2b536bc68bc5547c28da94368d148c600bfa
Samuel Rooke <samuel.rooke@tuni.fi>
2026-03-05 14:26:58 +0200

    refactor(menu): change imports and replace  with specific event listeners

--- app/src/main/java/tictactoe/Menu.java
+++ app/src/main/java/tictactoe/Menu.java
@@ -2,7 +2,9 @@ package tictactoe;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  * Menu is the main class for the game tic-tac-toe.
  *
